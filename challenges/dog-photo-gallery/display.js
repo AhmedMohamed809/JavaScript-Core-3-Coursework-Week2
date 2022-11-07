48d03d44cfac4babaa98a86fb96cc8acdecef3ee
Ahmed Mohamed <105979851+AhmedMohamed809@users.noreply.github.com>
2022-11-07 21:56:51 +0000

    Add comments

--- challenges/dog-photo-gallery/display.js
+++ challenges/dog-photo-gallery/display.js
@@ -1,21 +1,21 @@
 const buttonDisplay = document.getElementById("displayButton");
 const body = document.getElementsByName("body");
 buttonDisplay.addEventListener("click", (nextImg) => {
-  fetch("https://dog.ceo/api/breeds/image/randomcsd")
-    .then((res) => res.json())
+  fetch("https://dog.ceo/api/breeds/image/randomcsd") // fetch the API
+    .then((res) => res.json()) // convert data to  use JSON
     .then((data) => getImgesFromApi(data))
     .catch((err) => displayError(err));
 });
-
+// get and create random Img from API
 function getImgesFromApi(data) {
-  const nextImg = document.createElement("img");
+  const nextImg = document.createElement("img"); // create  img tag
   const listOfImg = document.getElementById("listImg");
-  const oneOfImgBylist = document.createElement("li");
-  nextImg.setAttribute("src", data.message);
+  const oneOfImgBylist = document.createElement("li"); // create  li tag
+  nextImg.setAttribute("src", data.message); // set prameters to IMG tag
   listOfImg.appendChild(oneOfImgBylist);
   oneOfImgBylist.appendChild(nextImg);
 }
-
+// display error massge
 function displayError(err) {
   console.log(err);
   const massg = document.createElement("h1");
--- challenges/programmer-humour/display.js
+++ challenges/programmer-humour/display.js
@@ -1,15 +1,17 @@
 const body = document.getElementById("body");
 fetch("https://xkcd.now.sh/?comic=latest")
-  .then((res) => res.json())
+  .then((res) => res.json()) // convert data to  use JSON
   .then((data) => getImgesFromApi(data))
   .catch((err) => {
+    // display error massge
     const displayErr = document.createElement("h1");
     displayErr.innerText = `wOOps there are some Error : ${err}`;
     body.appendChild(displayErr);
   });
+// get img from data in API
 function getImgesFromApi(data) {
   console.log(data);
-  const getImg = document.createElement("img");
+  const getImg = document.createElement("img"); // create  img tag
   getImg.setAttribute("src", data.img);
   body.appendChild(getImg);
 }
